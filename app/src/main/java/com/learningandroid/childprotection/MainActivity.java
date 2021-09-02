49c3454c838030d85955ea801317762ae5e0fc54
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-09-02 14:58:37 +0530

    First part of the project is done. Things added : 1 Home screen. 2 Recycler view screen.
    Basic structure of the app is made. The backend part is left, after that firebase part.

--- app/src/main/java/com/learningandroid/childprotection/MainActivity.java
+++ app/src/main/java/com/learningandroid/childprotection/MainActivity.java
@@ -3,19 +3,38 @@ package com.learningandroid.childprotection;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.learningandroid.childprotection.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        binding = DataBindingUtil.setContentView(MainActivity.this , R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+        btn = (Button) binding.ParentButton;
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callrecyclerView();
+            }
+        });
+
+    }
 
 
+    public void callrecyclerView() {
+        Intent intent = new Intent(this,recyclerView.class);
+        startActivity(intent);
     }
 }
