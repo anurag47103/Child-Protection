c0ed4b1b1604c971a63a5e28ea7860c60a4fc3b6
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-09-02 13:15:59 +0530

    the class recycler view is also done only left is to intent it from the main class

--- app/src/main/java/com/learningandroid/childprotection/recyclerView.java
+++ app/src/main/java/com/learningandroid/childprotection/recyclerView.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class recyclerView extends AppCompatActivity {
@@ -19,5 +20,36 @@ public class recyclerView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        initData();
+        initRecyclerView();
+    }
+
+    private void initData() {
+        userList = new ArrayList<>();
+        userList.add(new ModelClass(R.drawable.ap,"Amazon Prime","04:23"));
+        userList.add(new ModelClass(R.drawable.fb,"Facebook","05:03"));
+        userList.add(new ModelClass(R.drawable.fm,"Messenger","06:20"));
+        userList.add(new ModelClass(R.drawable.hs,"HotStar","01:12"));
+        userList.add(new ModelClass(R.drawable.ig,"Instagram","01:56"));
+        userList.add(new ModelClass(R.drawable.nf,"Netflix","00:54"));
+        userList.add(new ModelClass(R.drawable.pubg,"PUBG mobile","05:21"));
+        userList.add(new ModelClass(R.drawable.sc,"Snapchat","03:00"));
+        userList.add(new ModelClass(R.drawable.tg,"Telegram","00:55"));
+        userList.add(new ModelClass(R.drawable.wa,"Whatsapp","06:32"));
+        userList.add(new ModelClass(R.drawable.yt,"Youtube","09:23"));
+
+
+
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new Adapter(userList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
