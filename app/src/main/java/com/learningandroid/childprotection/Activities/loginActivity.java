a72e884be4e10e90380435b5ae76020abc11274d
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-10-21 21:09:25 +0530

    21-10-2021

--- app/src/main/java/com/learningandroid/childprotection/Activities/loginActivity.java
+++ app/src/main/java/com/learningandroid/childprotection/Activities/loginActivity.java
@@ -1,29 +1,17 @@
 package com.learningandroid.childprotection.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.learningandroid.childprotection.R;
 
 public class loginActivity extends AppCompatActivity {
-
-/*
-* This is the home screen here we are having two buttons 1 Child 2 parent
-* On parent button clicked we are going to the new activity where we are having the
-* recycler view;
-* */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        findViewById(R.id.ParentButton).setOnClickListener(View ->{
-            callrecyclerView();
-        });
-
+        findViewById(R.id.ParentButton).setOnClickListener(View -> callrecyclerView());
 
     }
 
