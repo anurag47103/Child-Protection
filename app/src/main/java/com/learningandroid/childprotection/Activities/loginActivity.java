01c4773b26b7621fb0a7cdedbd451284ab7d21a0
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-11-09 16:13:23 +0530

    otpActivity

--- app/src/main/java/com/learningandroid/childprotection/Activities/loginActivity.java
+++ app/src/main/java/com/learningandroid/childprotection/Activities/loginActivity.java
@@ -18,59 +18,21 @@ import java.util.concurrent.TimeUnit;
 
 public class loginActivity extends AppCompatActivity {
 
-    private EditText phone,otp;
-    private Button signin;
-    private Boolean getOtp;
-    private Editable phoneNumber;
-
+    private EditText phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        init();
-        signin.setText("Get Otp");
-        otp.setAlpha(0);
-
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(getOtp){
-                    phoneNumber= phone.getText();
-                    if(phoneNumber.toString().length()>9) {
-                        getOtp = false;
-                        signin.setText("Submit");
-                        otp.animate().alpha(1).setDuration(500);
-                        phone.setActivated(false);
-                        getotp(phoneNumber.toString());
-                    }
-                    else Toast.makeText(loginActivity.this, "Enter a valid number", Toast.LENGTH_SHORT).show();
-                }else{
-                    //authanticate
-                    callrecyclerView();
-                }
-            }
+        phone = findViewById(R.id.phonenumber);
+        findViewById(R.id.otp).setOnClickListener(view -> {
+           String s = phone.getText().toString();
+           if(s.length()==9){
+               Intent i = new Intent(loginActivity.this,otpActivity.class);
+               startActivity(i);
+           }else Toast.makeText(this,"Please enter a valid phone no.", Toast.LENGTH_SHORT).show();
         });
 
-    }
-
-    private void getotp(String s){
-
-    }
-
-    private void init(){
-        phone = findViewById(R.id.phonenumber);
-        otp = findViewById(R.id.otp);
-        signin = findViewById(R.id.ParentButton);
-        getOtp = true;
-
-    }
-
-    // this method is called when the parent button is pressed
-    public void callrecyclerView() {
-        Intent intent = new Intent(this, statsView.class);
-        startActivity(intent);
-        finish();
     }
 }
--- app/src/main/java/com/learningandroid/childprotection/Activities/otpActivity.java
+++ app/src/main/java/com/learningandroid/childprotection/Activities/otpActivity.java
@@ -2,15 +2,43 @@ package com.learningandroid.childprotection.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.learningandroid.childprotection.R;
 
 public class otpActivity extends AppCompatActivity {
 
+    private EditText otp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+
+        otp = findViewById(R.id.otp);
+        findViewById(R.id.verify).setOnClickListener(view -> {
+            if(verify()){
+
+            }else {
+                Toast.makeText(this, "Please enter valid otp", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean verify() {
+
+        return true;
+    }
+
+
+    public void callrecyclerView() {
+        Intent intent = new Intent(this, statsView.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
     }
 }
